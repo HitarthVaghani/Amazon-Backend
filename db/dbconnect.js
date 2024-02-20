5e1a20829e03daf666d4eaf88be05d32098bcd7a
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-20 18:20:20 +0530

    Update dbconnect.js

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
+
+
+//----------------- This is mongoDB server connection ---------------//
+
 const connectDB = () => {
   mongoose
     .connect("mongodb://127.0.0.1:27017/flipkart")

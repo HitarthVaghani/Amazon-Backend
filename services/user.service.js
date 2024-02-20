8e830726d3a4b6ebbe5d2922d7b4e6a8d7d045b8
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-20 18:43:46 +0530

    Update user.service.js

--- services/user.service.js
+++ services/user.service.js
@@ -1,8 +1,16 @@
 const { userSchema } = require("../models");
 
+
+
+//----------------- userExist request to check user exist or not  ---------------//
+
 const userExist = (email) => {
   return userSchema.findOne({ email });
 };
+
+
+
+//----------------- add request to add new user ---------------//
 
 const addUser = (body) => {
   const user = new userSchema({
@@ -16,13 +24,25 @@ const addUser = (body) => {
   return user.save(body);
 };
 
+
+
+//----------------- get request to get all user ---------------//
+
 const getUser = () => {
   return userSchema.find();
 };
 
+
+
+//----------------- delete request to delete user ---------------//
+
 const deleteUser = (id) => {
   return userSchema.findByIdAndDelete(id);
 };
+
+
+
+//----------------- update request to edit user's data ---------------//
 
 const updateUser = (id, body) => {
   return userSchema.findByIdAndUpdate(id, body);

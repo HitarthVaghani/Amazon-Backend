2e2403f6cb87ac97e73458afa6b73a3a3f8fc2a3
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-20 18:41:23 +0530

    Update subcategory.service.js

--- services/subcategory.service.js
+++ services/subcategory.service.js
@@ -1,5 +1,9 @@
 const { subcategorySchema } = require("../models");
 
+
+
+//----------------- add request to add new subcategory ---------------//
+
 const addSubcategory = (body) => {
   const subcategory = new subcategorySchema({
     subcategoryname: body.subcategoryname,
@@ -10,13 +14,24 @@ const addSubcategory = (body) => {
   return subcategory.save(body);
 };
 
+
+
+//----------------- get request to get all subcategorys ---------------//
+
 const getSubcategory = () => {
   return subcategorySchema.find().populate("category");
 };
 
+
+//----------------- delete request to delete subcategory ---------------//
+
 const deleteSubcategory = (id) => {
   return subcategorySchema.findByIdAndDelete(id).populate("category");
 };
+
+
+
+//----------------- update request to edit subcategorys data ---------------//
 
 const updateSubcategory = (id, body) => {
   return subcategorySchema.findByIdAndUpdate(id, body).populate("category");

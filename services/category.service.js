88c4514af1f22d68dc960f285e6e01960e74e3c2
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-20 18:39:54 +0530

    Update category.service.js

--- services/category.service.js
+++ services/category.service.js
@@ -1,8 +1,15 @@
 const { categorySchema } = require("../models");
 
+
+//----------------- categoryExist to check email was registered or not ---------------//
+
 const categoryExist = (category) => {
   return categorySchema.findOne({ category });
 };
+
+
+
+//----------------- add request to add new category ---------------//
 
 const addCategory = (body) => {
   const category = new categorySchema({
@@ -13,13 +20,24 @@ const addCategory = (body) => {
   return category.save(body);
 };
 
+
+//----------------- get request to get all categorys ---------------//
+
 const getCategory = () => {
   return categorySchema.find();
 };
 
+
+
+//----------------- delete request to delete category ---------------//
+
 const deleteCategory = (id) => {
   return categorySchema.findByIdAndDelete(id);
 };
+
+
+
+//----------------- update request to edit category data ---------------//
 
 const updateCategory = (id, body) => {
   return categorySchema.findByIdAndUpdate(id, body);

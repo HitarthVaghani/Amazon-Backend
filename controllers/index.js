93c58ca666f6c0e3a3ce93af0c9aaf251ca920f9
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-21 10:57:16 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,2 @@
 module.exports.userController = require("./user.controller");
-module.exports.categoryController = require("./category.controller");
-module.exports.subcategoryController = require("./subcategory.controller");
+

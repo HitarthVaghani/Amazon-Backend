0fc38f86150202ab299ba699501f448111e86ba9
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-21 11:00:14 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,10 @@ const express = require("express");
 const routes = express.Router();
 
 const userRoute = require("./user.route");
-const categoryRoute = require("./category.route");
-const subcategoryRoute = require("./subcategory.route");
 
 
 //----------------- user route ---------------//
 
 routes.use("/user", userRoute);
-
-
-//----------------- category route ---------------//
-
-routes.use("/category", categoryRoute);
-
-
-//----------------- subcategory route ---------------//
-
-routes.use("/subcategory", subcategoryRoute);
 
 module.exports = routes;

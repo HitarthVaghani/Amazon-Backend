848db128e650b528d4941fb618d5a0e503be72c0
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-21 11:00:53 +0530

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,3 +1,1 @@
 module.exports.userService = require("./user.service");
-module.exports.categoryService = require("./category.service");
-module.exports.subcategoryService = require("./subcategory.service");

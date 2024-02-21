3c25766ebba8502d36792b5b4800c5a939175e7d
Hitarth <160587641+HitarthVaghani@users.noreply.github.com>
2024-02-21 10:59:13 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,2 @@
 module.exports.userSchema = require("./user.model");
-module.exports.categorySchema = require("./category.model");
-module.exports.subcategorySchema = require("./subcategory.model");
+
